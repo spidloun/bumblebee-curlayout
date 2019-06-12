eef08334fd878f6134779a764d0b62587fc62d6e
Richard Cesar <richard.cesar@netkeepers.cz>
2019-06-12 10:41:50 +0200

    Initial commit of module and installation Makefile

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+install: curlayout.py
+	install -m 644 curlayout.py /usr/share/bumblebee-status/bumblebee/modules
---
+++ curlayout.py
@@ -0,0 +1,15 @@
+
+import bumblebee.engine
+import bumblebee.output
+import bumblebee.util
+
+class Module(bumblebee.engine.Module):
+	def __init__(self, engine, config):
+		super(Module, self).__init__(engine, config, bumblebee.output.Widget(full_text=self.current_layout))
+		
+	def current_layout(self, widget):
+		try:
+			result = bumblebee.util.execute("xkblayout-state print %s")
+		except RuntimeError:
+			return "n/a"
+		return result
